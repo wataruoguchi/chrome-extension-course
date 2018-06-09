4c4bb877fc51957c94a5a27fde54fff3c93ea037
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2018-06-09 16:42:54 -0700

    send message

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 const delay = 3 * 1000;
-const ginpeiNotification = () => {
-  chrome.notifications.create(null, {
-    type: chrome.notifications.TemplateType.BASIC,
-    iconUrl: '/ginpei.png',
-    title: 'Yo!',
-    message: '日本語',
-  });
+const ginpeiNotification = (msg) => {
+	chrome.notifications.create(null, {
+		type: chrome.notifications.TemplateType.BASIC,
+		iconUrl: '/ginpei.png',
+		title: 'Yo!',
+		message: '日本語' + JSON.stringify(msg),
+	});
 };
 
-setTimeout(() => ginpeiNotification(), delay)
+const ginpeiTimeout = (msg) => setTimeout(() => ginpeiNotification(msg), delay)
 
+chrome.runtime.onMessage.addListener((message) => ginpeiTimeout(message))
--- popup.js
+++ popup.js
@@ -1,13 +1,6 @@
 document.querySelector('.yo').onclick = (event) => {
-  const delay = 3 * 1000;
-	const ginpeiNotification = () => {
-		chrome.notifications.create(null, {
-			type: chrome.notifications.TemplateType.BASIC,
-			iconUrl: '/ginpei.png',
-			title: 'Yo!',
-			message: '日本語',
-		});
-	};
-
-	setTimeout(() => ginpeiNotification(), delay)
+	const message = {
+		type: 'start'
+	}
+	chrome.runtime.sendMessage(message)
 };
